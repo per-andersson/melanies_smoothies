1430fd791330496801e83e99c1a809fbaaf89b47
per-andersson <32965249+per-andersson@users.noreply.github.com>
2025-04-09 17:27:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARC
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.datafram(pd_df)
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
